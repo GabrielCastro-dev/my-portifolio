8767808ef09697625b7d63cfd289ebfd389f37eb
gabriel <gabrielcastro.ifpr@gmail.com>
2022-11-12 13:29:24 -0300

    default page design

--- src/layout/DefaultPage/DefaultPage.jsx
+++ src/layout/DefaultPage/DefaultPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-import './DefaultPage.css';
+import './DefaultPage.scss';
 
 function DefaultPage({ children }) {
   return (
