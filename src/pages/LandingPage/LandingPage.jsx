d1772bee3178c926835d25c6c0297aa2ca4bfb52
gabriel <gabrielcastro.ifpr@gmail.com>
2022-11-24 23:45:50 -0300

    button styling

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
 import Banner from '../../components/Banner/Banner';
+import ImgAndTxt from '../../components/ImgAndTxt/ImgAndTxt';
 
 import './LandingPage.scss';
 
@@ -8,6 +9,7 @@ function LandingPage() {
   return (
     <DefaultPage>
       <Banner />
+      <ImgAndTxt />
     </DefaultPage>
   )
 }
