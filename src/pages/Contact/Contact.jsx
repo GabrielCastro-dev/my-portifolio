9d2f4dff16934eda1164e333f46db17427c3fca5
gabriel <gabrielcastro.developer@gmail.com>
2023-03-22 20:50:11 -0300

    Blob updates

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,17 @@
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
+import Blob from '../../components/Blob/Blob';
 import './Contact.scss';
 
 function Contact(){
   return(
     <DefaultPage>
       <div className='contact'>
+        <Blob
+          width='500px'
+          height='350px'
+          top='-100px'
+          left='-100px'
+        />
         <div className="left">
           <h1>ENTRE EM</h1>
           <h1 className='empty'>CONTATO</h1>
