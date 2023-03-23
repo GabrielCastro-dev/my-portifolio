95e1de3391153680afc570e824b6c3c68d44f146
gabriel <gabrielcastro.developer@gmail.com>
2023-03-22 21:17:27 -0300

    email functionality

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,24 @@
+import { useRef, useState } from 'react';
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
 import Blob from '../../components/Blob/Blob';
+import emailjs from '@emailjs/browser';
 import './Contact.scss';
 
 function Contact(){
+  const form = useRef();
+
+  function sendEmail(event){
+    event.preventDefault();
+
+    emailjs.sendForm('contact_email', 'template_6gnxxo6', form.current, 'uyXISEL1Oy6E6pO8K')
+      .then((result) => {
+          event.target.reset();
+          alert('Email enviado com sucesso, entraremos em contato.');
+      }, (error) => {
+          alert('Erro ao enviar mensagem, por favor tente mais tarde.');
+      });
+  }
+
   return(
     <DefaultPage>
       <div className='contact'>
@@ -12,19 +28,29 @@ function Contact(){
           top='-100px'
           left='-100px'
         />
+
         <div className="left">
           <h1>ENTRE EM</h1>
           <h1 className='empty'>CONTATO</h1>
         </div>
-        <form>
+
+        <form ref={form} onSubmit={sendEmail}>
           <div>
-            <input type="text" placeholder='Nome'/>
-            <input type="text" placeholder='Email'/>
+            <input required name='name' type="text" placeholder='Nome'/>
+            <input required name='email' type="text" placeholder='Email'/>
           </div>
-          <input type="text" placeholder='Assunto'/>
-          <textarea type="text" placeholder='Mensagem'/>
-          <button>Enviar</button>
+          <input required name='subject' type="text" placeholder='Assunto'/>
+          <textarea required  name='message' type="text" placeholder='Mensagem'/>
+
+          <input type='submit' value='Enviar' className='send-button'/> 
         </form>
+
+        <Blob
+          width='500px'
+          height='350px'
+          top='500px'
+          right='-100px'
+        />
       </div>
     </DefaultPage>
   )
