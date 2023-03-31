8fa9b54065e57d3d582929f38e582c91dbe26124
gabriel <gabrielcastro.developer@gmail.com>
2023-03-30 22:13:25 -0300

    eliminating unused useState

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
 import Blob from '../../components/Blob/Blob';
 import emailjs from '@emailjs/browser';
