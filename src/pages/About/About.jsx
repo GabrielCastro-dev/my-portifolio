1921362e7eb6499dacf1d5e514ca7eb6ab3621ac
gabriel <gabrielcastro.developer@gmail.com>
2023-03-30 22:21:05 -0300

    page title on about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
+import Pagetitle from '../../components/PageTitle/PageTitle';
 import './About.scss';
 
 function About() {
   return (
     <DefaultPage>
+      <Pagetitle title='SOBRE' subtitle='MIM' />
       
     </DefaultPage>
   )
