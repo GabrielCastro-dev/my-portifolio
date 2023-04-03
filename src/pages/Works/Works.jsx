6bf60325c6df7cddd681d64495b52dbcc5935295
gabriel <gabrielcastro.developer@gmail.com>
2023-04-03 17:40:54 -0300

    mockup added

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,11 +1,18 @@
 import DefaultPage from '../../layout/DefaultPage/DefaultPage';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import mockup from '../../assets/mockup.png';
+import Project from '../../components/Project/Project';
 import './Works.scss';
 
 function Works() {
   return (
     <DefaultPage>
       <PageTitle title='TRABALHOS' subtitle='REALIZADOS' />
+      <Project 
+        image={mockup}
+        title='Título do Projeto' 
+        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget auctor felis. Maecenas id lacus libero. Sed ac sem tempor, sollicitudin eros sed, dignissim mi. Curabitur tincidunt diam vel augue sollicitudin, ac pulvinar est vestibulum.'
+      />
     </DefaultPage>
   )
 }
