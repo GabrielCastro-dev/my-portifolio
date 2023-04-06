cf2341db48070f9005b1db9f0a337b05f9146fc0
gabriel <gabrielcastro.developer@gmail.com>
2023-04-06 17:39:26 -0300

    prject styling

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,13 +1,16 @@
+import Button from '../Button/Button';
 import './Project.scss';
 
 function Project({image, title, description}) {
     return (
-        <div className="project">
-            <img src={image} alt="Imagem do Projeto" />
-            <div>
-                <h2>{title}</h2>
-                <p>{description}</p>
-                <button>Saiba mais</button>
+        <div className='project-background'>
+            <div className="project">
+                <img src={image} alt="Imagem do Projeto" />
+                <div>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <Button>Saiba mais</Button>
+                </div>
             </div>
         </div>
     )
